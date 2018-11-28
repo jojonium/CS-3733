5b16b3f6c72e84e45291299850f12584bfd87f0b
Joseph Petitti <jojonium@autoreg-072019.dyn.wpi.edu>
2018-11-28 04:23:15 -0500

    added easter egg and more scripts

--- client side/scripts/index.js
+++ client side/scripts/index.js
@@ -2,15 +2,18 @@ const createScheduleUrl = "assblast.me";
 
 var handleGetSchedule = function(e) {                                            
 	var form = document.enterCodeForm;                                       
-	var arg1 = form.code.value;                                              
-	var data = {};                                                           
-	data["scheduleID"] = arg1;                                               
-	var js = JSON.stringify(data);                                           
-	console.log("JS: " + js);
+	var arg1 = form.sid.value;                                              
+	console.log(arg1);
+	if (arg1.toLowerCase() == "cs3733") {
+		window.open('klotski.html', '_self', false);
+		return;
+	}
 	var xhr = new XMLHttpRequest();
  };
 
 var handleCreateSchedule = function(e) {
+	var incFlag = false;
+	$("#create-box form table tr td input").css("border-color", "initial")
 	var form = document.createScheduleForm;
 	var sdateArg = form.sdate.value;
 	var edateArg = form.edate.value;
@@ -18,6 +21,27 @@ var handleCreateSchedule = function(e) {
 	var etimeArg = form.etime.value;
 	var nameArg = form.name.value;
 	var durationArg = form.duration.value;
+	if (sdateArg == "") {
+		$("#start-date").css("border-color", "red")
+		incFlag = true;
+	}
+	if (edateArg == "") {
+		$("#end-date").css("border-color", "red");
+		incFlag = true;
+	}
+	if (stimeArg == "") {
+		$("#stime").css("border-color", "red");
+		incFlag = true;
+	}
+	if (etimeArg == "") {
+		$("#etime").css("border-color", "red");
+		incFlag = true;
+	}
+	if (nameArg == "") {
+		$("#name").css("border-color", "red");
+		incFlag = true;
+	}
+	if (incFlag) return; // stop if form isn't completely filled out
 	var data = {};
 	data["name"] = nameArg;
 	data["startDate"] = sdateArg;
@@ -26,7 +50,7 @@ var handleCreateSchedule = function(e) {
 	data["endTime"] = etimeArg;
 	data["duration"] = durationArg;
 	var json = JSON.stringify(data);
-	console.log("JSON: " + json);
+	alert("JSON: " + json);
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", createScheduleUrl, true);
 

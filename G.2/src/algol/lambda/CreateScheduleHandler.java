c18da5627ba963347e747ee385f20e0a0a9ee7d0
Matthew Hagan <subbassbro@gmail.comn>
2018-11-28 12:00:34 -0500

    staging

--- G.2/src/algol/lambda/CreateScheduleHandler.java
+++ G.2/src/algol/lambda/CreateScheduleHandler.java
@@ -19,6 +19,7 @@ import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 import com.google.gson.Gson;
 

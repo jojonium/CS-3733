b33c7ee08202b5ccf2ce5b044199e822a899e537
Joseph Petitti <jojonium@autoreg-072019.dyn.wpi.edu>
2018-11-28 22:21:19 -0500

    Moved to new Eclipse project

---
+++ Algol/src/main/java/edu/wpi/cs/algol/lambda/CreateScheduleRequest.java
@@ -0,0 +1,22 @@
+package edu.wpi.cs.algol.lambda;
+public class CreateScheduleRequest {
+	String name;
+	String dateStart;
+	String dateEnd;
+	String timeStart;
+	String timeEnd;
+	String duration;
+	
+	public CreateScheduleRequest (String name, String dateStart, String dateEnd, String timeStart, String timeEnd,  String duration) {
+		this.name = name;
+		this.dateStart = dateStart;
+		this.dateEnd = dateEnd;
+		this.timeStart = timeStart;
+		this.timeEnd = timeEnd;
+		this.duration = duration;
+	}
+	
+	public String toString() {
+		return "Create("+ name + "," + dateStart + "," + dateEnd + "," + timeStart + "," + timeEnd + "," + duration +  ")";
+	}
+}
